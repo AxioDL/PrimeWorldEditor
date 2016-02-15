dfd9ea92db09fc17fd7949ce40e4457d92b97494
parax0 <parax0@github.com>
2016-02-15 06:20:34 -0700

    Overhauled CompressionUtil

--- src/Common/CompressionUtil.h
+++ src/Common/CompressionUtil.h
@@ -1,11 +1,26 @@
 #ifndef COMPRESSIONUTIL_H
 #define COMPRESSIONUTIL_H
 
+#include "TString.h"
 #include "types.h"
+#include <FileIO/FileIO.h>
 
 namespace CompressionUtil
 {
-    bool DecompressAreaLZO(u8 *src, u32 src_len, u8 *dst, u32 dst_len);
+    TString ErrorText_zlib(s32 Error);
+    TString ErrorText_LZO(s32 Error);
+
+    // Decompression
+    bool DecompressZlib(u8 *pSrc, u32 SrcLen, u8 *pDst, u32 DstLen, u32& rTotalOut);
+    bool DecompressLZO(u8 *pSrc, u32 SrcLen, u8 *pDst, u32& rTotalOut);
+    bool DecompressSegmentedData(u8 *pSrc, u32 SrcLen, u8 *pDst, u32 DstLen);
+
+    // Compression
+    bool CompressZlib(u8 *pSrc, u32 SrcLen, u8 *pDst, u32 DstLen, u32& rTotalOut);
+    bool CompressLZO(u8 *pSrc, u32 SrcLen, u8 *pDst, u32& rTotalOut);
+    bool CompressSegmentedData(u8 *pSrc, u32 SrcLen, u8 *pDst, u32& rTotalOut, bool IsZlib);
+    bool CompressZlibSegmented(u8 *pSrc, u32 SrcLen, u8 *pDst, u32& rTotalOut);
+    bool CompressLZOSegmented(u8 *pSrc, u32 SrcLen, u8 *pDst, u32& rTotalOut);
 }
 
 #endif // COMPRESSIONUTIL_H
