a2762e034ea99410c6b1a9f36f87f4085f7bddb8
parax0 <parax0@github.com>
2017-01-11 08:30:40 -0700

    Lots of collision surface color tweaking

--- src/Core/Resource/CCollisionMaterial.cpp
+++ src/Core/Resource/CCollisionMaterial.cpp
@@ -43,20 +43,27 @@ ECollisionFlag CCollisionMaterial::SurfaceType(EGame Game) const
 
 // Type-to-color mappings
 const std::map<ECollisionFlag, CColor> gkTypeToColor = {
-    { eCF_Stone,        CColor::Integral(220, 215, 160) }, // Brownish/greenish color
-    { eCF_Metal,        CColor::Integral(110, 110, 110) }, // Dark gray
-    { eCF_Grass,        CColor::Integral( 90, 150,  70) }, // Green
-    { eCF_Ice,          CColor::Integral(200, 255, 255) }, // Light blue
-    { eCF_MetalGrating, CColor::Integral(170, 170, 170) }, // Gray
-    { eCF_Phazon,       CColor::Integral(  0, 128, 255) }, // Blue
-    { eCF_Dirt,         CColor::Integral(150, 130, 120) }, // Brownish-gray
-    { eCF_Lava,         CColor::Integral(200,  30,  30) }, // Red
-    { eCF_Snow,         CColor::Integral(230, 255, 255) }, // *Very* light blue
-    { eCF_Glass,        CColor::Integral( 20, 255, 190) }, // Greenish blue
-    { eCF_Shield,       CColor::Integral(230, 250,  60) }, // Yellow
-    { eCF_Sand,         CColor::Integral(230, 200, 170) }, // Light brown
-    { eCF_Wood,         CColor::Integral(190, 140, 105) }, // Brown
-    { eCF_Organic,      CColor::Integral(130, 130, 250) }  // Purple
+    { eCF_Stone,            CColor::Integral(220, 215, 160) }, // Brownish/greenish
+    { eCF_Metal,            CColor::Integral(143, 143, 143) }, // Gray
+    { eCF_Grass,            CColor::Integral( 90, 150,  70) }, // Green
+    { eCF_Ice,              CColor::Integral(200, 255, 255) }, // Light blue
+    { eCF_MetalGrating,     CColor::Integral(180, 180, 180) }, // Gray
+    { eCF_Phazon,           CColor::Integral(  0, 128, 255) }, // Blue
+    { eCF_Dirt,             CColor::Integral(107,  84,  40) }, // Brown
+    { eCF_Lava,             CColor::Integral(200,  30,  30) }, // Red
+    { eCF_AltMetal,         CColor::Integral(100, 100, 100) }, // Gray
+    { eCF_Snow,             CColor::Integral(230, 255, 255) }, // *Very* light blue
+    { eCF_Fabric,           CColor::Integral( 64, 133, 236) }, // Blue
+    { eCF_SlowMud,          CColor::Integral(109,  91,  66) }, // Brown
+    { eCF_Mud,              CColor::Integral(121, 106,  84) }, // Brown
+    { eCF_Glass,            CColor::Integral(172, 209, 254) }, // Greenish blue
+    { eCF_Shield,           CColor::Integral(230, 250,  60) }, // Yellow
+    { eCF_Sand,             CColor::Integral(225, 188, 133) }, // Light brown
+    { eCF_MothSeedOrganics, CColor::Integral(172, 170, 202) }, // Light purple
+    { eCF_Web,              CColor::Integral(191, 176, 162) }, // Light grayish/brown
+    { eCF_Wood,             CColor::Integral(139,  90,  43) }, // Brown
+    { eCF_Organic,          CColor::Integral(130, 130, 250) }, // Purple
+    { eCF_Rubber,           CColor::Integral( 58,  58,  58) }  // Black
 };
 CColor CCollisionMaterial::SurfaceColor(EGame Game) const
 {
