53881d17482dcf59c1b4829762ba933b95bcc9a9
Lioncash <mathew1800@gmail.com>
2020-06-19 03:30:42 -0400

    CPoiToWorldLoader: Make use of unsigned stream utilities

--- src/Core/Resource/Factory/CPoiToWorldLoader.cpp
+++ src/Core/Resource/Factory/CPoiToWorldLoader.cpp
@@ -3,12 +3,12 @@
 std::unique_ptr<CPoiToWorld> CPoiToWorldLoader::LoadEGMC(IInputStream& rEGMC, CResourceEntry *pEntry)
 {
     auto pOut = std::make_unique<CPoiToWorld>(pEntry);
-    uint32 NumMappings = rEGMC.ReadLong();
+    const uint32 NumMappings = rEGMC.ReadULong();
 
     for (uint32 iMap = 0; iMap < NumMappings; iMap++)
     {
-        uint32 MeshID = rEGMC.ReadLong();
-        uint32 InstanceID = rEGMC.ReadLong() & 0x03FFFFFF;
+        const uint32 MeshID = rEGMC.ReadULong();
+        const uint32 InstanceID = rEGMC.ReadULong() & 0x03FFFFFF;
         pOut->AddPoiMeshMap(InstanceID, MeshID);
     }
 
