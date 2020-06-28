75f805f0ff262151d8f03e3f5187f73a5e0aad66
Lioncash <mathew1800@gmail.com>
2020-06-28 06:16:39 -0400

    CVirtualDirectoryTreeView: Make use of in-class initializers where applicable

--- src/Editor/ResourceBrowser/CVirtualDirectoryTreeView.h
+++ src/Editor/ResourceBrowser/CVirtualDirectoryTreeView.h
@@ -9,13 +9,14 @@ class CVirtualDirectoryTreeView : public QTreeView
 {
     Q_OBJECT
 
-    CVirtualDirectoryModel *mpModel;
-    bool mTransferSelectionPostMove;
+    CVirtualDirectoryModel *mpModel = nullptr;
+    bool mTransferSelectionPostMove = false;
 
 public:
-    CVirtualDirectoryTreeView(QWidget *pParent = 0);
-    void dragEnterEvent(QDragEnterEvent *pEvent);
-    void setModel(QAbstractItemModel *pModel);
+    explicit CVirtualDirectoryTreeView(QWidget *pParent = nullptr);
+
+    void dragEnterEvent(QDragEnterEvent *pEvent) override;
+    void setModel(QAbstractItemModel *pModel) override;
 
 public slots:
     void OnDirectoryAboutToBeMoved(CVirtualDirectory *pDir);
