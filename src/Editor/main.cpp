ac70f08768e09e692905d3803da48aa956f232fb
Lioncache <mai.iam2048@gmail.com>
2026-08-13 10:16:03 -0400

    Editor/main: Mark palette handling as static
    This doesn't touch instance state.

--- src/Editor/main.cpp
+++ src/Editor/main.cpp
@@ -190,7 +190,7 @@ public:
         NGameList::Shutdown();
     }
 
-    void SetupPalette()
+    static void SetupPalette()
     {
         QPalette DarkPalette;
         ConfigureColorGroup(DarkPalette, QPalette::Active, 100);
@@ -199,21 +199,21 @@ public:
         qApp->setPalette(DarkPalette);
     }
 
-    void ConfigureColorGroup(QPalette& Palette, QPalette::ColorGroup Group, int Factor)
+    static void ConfigureColorGroup(QPalette& Palette, QPalette::ColorGroup Group, int Factor)
     {
-        Palette.setColor( Group, QPalette::Window,           QColor(53,53,53)  .darker(Factor) );
-        Palette.setColor( Group, QPalette::WindowText,       QColor(Qt::white) .darker(Factor) );
-        Palette.setColor( Group, QPalette::Base,             QColor(25,25,25)  .darker(Factor) );
-        Palette.setColor( Group, QPalette::AlternateBase,    QColor(35,35,35)  .darker(Factor) );
-        Palette.setColor( Group, QPalette::ToolTipBase,      QColor(Qt::black) .darker(Factor) );
-        Palette.setColor( Group, QPalette::ToolTipText,      QColor(Qt::white) .darker(Factor) );
-        Palette.setColor( Group, QPalette::Text,             QColor(Qt::white) .darker(Factor) );
-        Palette.setColor( Group, QPalette::Button,           QColor(53,53,53)  .darker(Factor) );
-        Palette.setColor( Group, QPalette::ButtonText,       QColor(Qt::white) .darker(Factor) );
-        Palette.setColor( Group, QPalette::BrightText,       QColor(Qt::red)   .darker(Factor) );
-        Palette.setColor( Group, QPalette::Link,             QColor(42,130,218).darker(Factor) );
-        Palette.setColor( Group, QPalette::Highlight,        QColor(42,130,218).darker(Factor) );
-        Palette.setColor( Group, QPalette::HighlightedText,  QColor(Qt::white) .darker(Factor) );
+        Palette.setColor(Group, QPalette::Window,           QColor(53,53,53)  .darker(Factor));
+        Palette.setColor(Group, QPalette::WindowText,       QColor(Qt::white) .darker(Factor));
+        Palette.setColor(Group, QPalette::Base,             QColor(25,25,25)  .darker(Factor));
+        Palette.setColor(Group, QPalette::AlternateBase,    QColor(35,35,35)  .darker(Factor));
+        Palette.setColor(Group, QPalette::ToolTipBase,      QColor(Qt::black) .darker(Factor));
+        Palette.setColor(Group, QPalette::ToolTipText,      QColor(Qt::white) .darker(Factor));
+        Palette.setColor(Group, QPalette::Text,             QColor(Qt::white) .darker(Factor));
+        Palette.setColor(Group, QPalette::Button,           QColor(53,53,53)  .darker(Factor));
+        Palette.setColor(Group, QPalette::ButtonText,       QColor(Qt::white) .darker(Factor));
+        Palette.setColor(Group, QPalette::BrightText,       QColor(Qt::red)   .darker(Factor));
+        Palette.setColor(Group, QPalette::Link,             QColor(42,130,218).darker(Factor));
+        Palette.setColor(Group, QPalette::Highlight,        QColor(42,130,218).darker(Factor));
+        Palette.setColor(Group, QPalette::HighlightedText,  QColor(Qt::white) .darker(Factor));
     }
 };
 
